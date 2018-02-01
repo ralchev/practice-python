f95dd714f029bd8faf313fe1991e7a7191ef8afc
Ivan Ralchev <iraltchev@gmail.com>
2018-02-01 22:40:31 +0200

    Fixed docstring

--- practice_python_exercise_6.py
+++ practice_python_exercise_6.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Ask the user for a string and print out whether this string is a palindrome or not. (A palindrome is a string that reads the same forwards and backwards.)
+Ask the user for a string and print out whether this string is a palindrome
+or not. (A palindrome is a string that reads the same forwards and backwards.)
 """
 
 word = str(input("Please give me a word!"))
