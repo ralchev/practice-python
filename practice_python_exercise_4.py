30faec7e5ad2cda502b7ed41830bdcad5d79a641
Ivan Ralchev <iraltchev@gmail.com>
2018-02-01 22:41:59 +0200

    Fixed docstring

--- practice_python_exercise_4.py
+++ practice_python_exercise_4.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Create a program that asks the user for a number and then prints out a list of all the divisors of that number. (If you don’t know what a divisor is, it is a number that divides evenly into another number. For example, 13 is a divisor of 26 because 26 / 13 has no remainder.)
+Create a program that asks the user for a number and then prints out a list
+of all the divisors of that number. (If you don’t know what a divisor is,
+it is a number that divides evenly into another number. For example,
+13 is a divisor of 26 because 26 / 13 has no remainder.)
 """
 
 number = int(input("Please give me a number!"))
