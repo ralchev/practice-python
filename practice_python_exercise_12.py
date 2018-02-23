216900e4b605363629f9138a85e6b5b8d2471c5b
Ivan Ralchev <iraltchev@gmail.com>
2018-02-23 21:43:04 +0200

    More list comprehension

---
+++ practice_python_exercise_12.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 23 21:36:41 2018
+
+@author: ralchev
+"""
+#import random
+# list = random.sample(range(1, 2000), 50)
+def list_ends(list):
+    return([list[0], list[len(list)-1]])
