4d6c195d62f88c091a2efc29e6d4e1c9a5ddd78d
Ivan Ralchev <iraltchev@gmail.com>
2018-02-23 21:32:00 +0200

    List comprehension rocks

---
+++ practice_python_exercise_11.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Ask the user for a number and determine whether the number is prime or not.
+"""
+
+def is_prime():
+    number = int(input("Please give me a number! Press Ctrl + C if you want to stop."))
+
+    list_of_divisors = [divisor for divisor in range(2, number) if number % divisor == 0]
+
+    if number > 1:
+        if len(list_of_divisors) == 0:
+            print(number, "is prime")
+        else:
+            print(number, "is not prime")
+    else:
+        print("Something is wrong here")
+        
+while True:
+    is_prime()
